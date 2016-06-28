90ae6a070b517ecabde9e8a46783f100516b82da
Alex Wong <wong.alex1121@gmail.com>
2016-06-28 16:27:18 -0700

    Test

---
+++ Character Control/Assets/Script/MouseAim.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseAim : AimingScript {
+    public Transform target;
+    public Transform targetObject;
+    public float distance;
+    public float angle;
+    // Use this for initialization
+    void Start () {
+        MousePosition = getMousePosition();
+        targetObject=(Transform) Instantiate(target, new Vector3(MousePosition.x, MousePosition.y, 1), Quaternion.identity);
+        MaxDistance = 10;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        distance = getDistancePlus();
+        angle = getAnglePlus();
+        if (distance <= MaxDistance)
+        {
+            targetObject.position = new Vector3(MousePosition.x, MousePosition.y, 0);
+        }
+        else {
+            targetObject.position = new Vector3(MaxDistance * Mathf.Cos(angle)+MainPlayer.transform.position.x, MaxDistance * Mathf.Sin(angle)+MainPlayer.transform.position.y, 0);
+        }
+    }
+
+
+}
---
+++ Character Control/Assets/Script/Parent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parent : MonoBehaviour
+{
+	public GameObject player;
+
+	void Start(){
+		parenting (transform.parent.gameObject);
+		player.transform.position = transform.parent.gameObject.transform.position;
+	}
+	//Invoked when a button is pressed.
+	public void parenting(GameObject newParent)
+	{
+		//Makes the GameObject "newParent" the parent of the GameObject "player".
+		player.transform.parent = newParent.transform;
+
+		//Display the parent's name in the console.
+		Debug.Log ("Player's Parent: " + player.transform.parent.name);
+
+		// Check if the new parent has a parent GameObject.
+		if(newParent.transform.parent != null)
+		{
+			//Display the name of the grand parent of the player.
+			Debug.Log ("Player's Grand parent: " + player.transform.parent.parent.name);
+		}
+	}
+}
