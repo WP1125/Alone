c36dd157c899d87a9142acd4e17e71ec2dcc8e58
Quinn Levine <Qlevine@uci.edu>
2016-10-20 18:25:02 -0700

    Basically moved everything from puzzles to Develop branch, we can fix any merge conflicts after.

---
+++ Character Control/Assets/Script/BoxSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxSpawner : MonoBehaviour {
+
+
+    public GameObject Box;
+    public float maxWait = 5;
+    public float minWait = 1;
+    public int maxBoxCount = 5;
+    private float currWait = 0;
+    private float spawnBox = 0;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currWait+= Time.deltaTime;
+        if(currWait > spawnBox && transform.childCount < maxBoxCount)
+        {
+            currWait = 0;
+            spawnBox = Random.Range(minWait, maxWait);
+            GameObject SpawnedBox = Instantiate(Box, transform.position, Quaternion.identity) as GameObject;
+            SpawnedBox.transform.parent = transform;
+        }
+	}
+}
---
+++ Character Control/Assets/Script/ExplosiveController.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class ExplosiveController : MonoBehaviour {
+
+    //public GameObject targetExplosive;
+    public BasePlayer player;
+    public CircleCollider2D impactArea;
+    public GameObject ExplosionPart;
+
+    public int itemHP = 20;
+    public int explosionDamage;
+    
+
+    public float explosionTimer = 0.5f;
+    private float explosionTime;
+    private bool explodeSignal;
+    private bool playerInRange;
+    private bool exploding = false;
+    private GameObject counter;
+
+    public bool startTime = false;
+    
+
+
+    void Start () {
+        playerInRange = false;
+        impactArea.enabled = false;
+        explodeSignal = false;
+        counter = transform.parent.FindChild("Timer").gameObject;
+        counter.SetActive(false);
+    }
+	
+	void Update () {
+	    if (itemHP <= 20)
+        {
+            
+            impactArea.enabled = true;
+
+            if (explodeSignal == false && startTime)
+            {
+                explodeSignal = true;
+                explosionTime = Time.time + explosionTimer;
+                
+                counter.SetActive(true);
+                
+            }
+
+
+
+
+            counter.GetComponent<Text>().text = (Convert.ToInt32(explosionTime-Time.time)).ToString();
+
+
+            if (explosionTime <= Time.time && startTime)
+            {
+                explode();
+            }
+        }
+
+	}
+
+    void explode()
+    {
+        if (playerInRange)
+        {
+            player.deceaseHP(explosionDamage);
+        }
+        Destroy(gameObject.transform.parent.gameObject);
+        Instantiate(ExplosionPart, transform.position, Quaternion.identity);
+        exploding = true;
+        
+        
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            playerInRange = true;
+        }
+
+        if (exploding)
+        {
+            if (other.gameObject.tag == "Break")
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            playerInRange = true;
+        }
+
+        if (exploding)
+        {
+            if (other.gameObject.tag == "Break")
+            {
+                Destroy(other.gameObject);
+            }
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+}
