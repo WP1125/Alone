a86a0b24ca159527db70cb466590c05629bd1a94
Quinn Levine <Qlevine@uci.edu>
2016-10-27 14:34:25 -0700

    Added new scenes to character control, all scripts must be reattched because they did not import properly to the new folder.  Only changed Character Control so as not to interfere with other work.

---
+++ Character Control/Assets/Script/InventoryController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryController : MonoBehaviour {
+
+    public GameObject inventory;
+    public GameObject player;
+
+	void Start () {
+        inventory.SetActive(false);
+	}
+	
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.I))
+        {
+            inventory.SetActive(!inventory.activeSelf);
+        }
+
+        //player.GetComponent<Player>().enabled = !inventory.activeSelf;
+    }
+}
